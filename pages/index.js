88bca0db4e6d71fe82a75e343c1a3c652dc0c61a
jjimenezn-Dev <36519001+jjimenezn-Dev@users.noreply.github.com>
2021-05-12 20:18:00 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Neuropsicología</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 Style="text-align:center; margin:2% 0%;">Neuropsicología Lulito</h1>
+      <img src="https://i.pinimg.com/originals/b1/65/5a/b1655aff5147ab54551507efe06b91fd.gif" Style='margin: 2% 25%;width: 50%;'></img>
+    </div>
+  )
+}
